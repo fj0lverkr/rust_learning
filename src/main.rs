73e4a3829b39d89908ce629e561ed7e8ee041816
fj0lverkr <nils.nahooy@live.com>
2020-12-19 17:47:03 +0100

    started guessing game tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+/*
 use ferris_says::say; // from the previous step
 use std::io::{stdout, BufWriter};
 
@@ -8,4 +9,21 @@ fn main() {
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+}
+*/
+
+use std::io;
+
+fn main() {
+    println!("Guessing game");
+
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("You guessed {}!", guess)
 }
