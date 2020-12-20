ae4d7e328abc94173fd4ce2ccb838fd0333b6262
fj0lverkr <nils.nahooy@live.com>
2020-12-20 10:44:56 +0100

    created game loop

--- src/main.rs
+++ src/main.rs
@@ -23,23 +23,27 @@ fn main() {
     println!("Secret number is {}.", &secret_number);
 
     println!("Guessing game");
+    loop {
+        println!("Please input your guess:");
 
-    println!("Please input your guess:");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please type a number!");
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a number!");
-
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Your guess is too low!"),
-        Ordering::Equal => println!("Your guess is correct!"),
-        Ordering::Greater => println!("Your guess is too big!")
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Your guess is too low!"),
+            Ordering::Equal => {
+                println!("Your guess is correct!");
+                break;
+            }
+            Ordering::Greater => println!("Your guess is too big!")
+        }
     }
 }
